657f3f6462cfd862ebb459e35a625b0ec8125c3a
Ryan Culpepper <ryanc@racket-lang.org>
2019-11-24 13:34:02 +0100

    update struct and data defns

--- macro-debugger-text-lib/macro-debugger/model/deriv-c.rkt
+++ macro-debugger-text-lib/macro-debugger/model/deriv-c.rkt
@@ -1,179 +1,197 @@
 #lang racket/base
 (provide (all-defined-out))
 
-;; PrepareExpEnv = (listof LocalAction)
+;; For brevity, the type (Listof X) is abbreviated Xs. For example,
+;; (Listof Stx) is written Stxs.
 
-;; A Node(a) is:
-;;   (make-node a ?a)
-(define-struct node (z1 z2) #:transparent)
+;; Because of errors, any node might have missing fields after an
+;; error or an interrupted sub-node. For example, consider the type
+;; declaration for MRule:
+;;   (mrule Stx Stx Ids ?Exn Stx LocalActions ?Exn Stx Deriv)
+;; The ?Exn means an Exn value is present iff the subsequent fields
+;; are skipped (ie, have #f or other default values). Furthermore, the
+;; LocalActions type might also be interrupted, which means that the
+;; fields following it would be skipped.
 
-;; A TopDeriv is one of
-;;   (make-lift-deriv <Node(Stx)> Deriv Stxs TopDeriv)
-;;   Deriv
+;; type Node(a) = (node a ?a)
+(struct node (z1 z2) #:transparent)
 
-;; A Deriv is one of
-;;   MRule
-;;   PrimDeriv
+;; type Deriv = (deriv Stx Stx) <: Node(Stx)
+;; type Base = (base Stx Stx Ids ?Exn) <: Deriv
+(struct deriv node () #:transparent)
+(struct base deriv (resolves ?1) #:transparent)
 
-;; Base = << Node(Stx) Rs ?exn >>
+;; A LiftDeriv is (lift-deriv Stx Stx Deriv Stxs Deriv)
+(struct lift-deriv deriv (first lift-stx second) #:transparent)
 
-(define-struct (deriv node) () #:transparent)
-(define-struct (base deriv) (resolves ?1) #:transparent)
+;; A TagRule is (tagrule Stx Stx Stx Deriv)
+(struct tagrule deriv (tagged-stx next) #:transparent)
 
-(define-struct (lift-deriv deriv) (first lift-stx second) #:transparent)
-(define-struct (tagrule deriv) (tagged-stx next) #:transparent)
+;; A DerivLL is one of Deriv | (lift/let-deriv Stx Stx Deriv Stx DerivLL)
+(struct lift/let-deriv deriv (first lift-stx second) #:transparent)
 
-;; A DerivLL is one of
-;;   (make-lift/let-deriv <Node(Stx)> Deriv Stx Deriv)
-;;   Deriv
-(define-struct (lift/let-deriv deriv) (first lift-stx second) #:transparent)
 
-;; A MRule is
-;;   (make-mrule <Base(Stx)> ?Stx (listof LocalAction) ?exn ?Stx ?Deriv)
-(define-struct (mrule base) (me1 locals me2 ?2 etx next) #:transparent)
+;; ============================================================
+;; Macros
+
+;; A MRule is (mrule <Base> Stx LocalActions ?Exn Stx Deriv)
+(struct mrule base (me1 locals me2 ?2 etx next) #:transparent)
 
 ;; A LocalAction is one of:
-(define-struct local-exn (exn) #:transparent)
-(define-struct (local-expansion node) (for-stx? me1 inner lifted me2 opaque)
-  #:transparent)
-(define-struct local-lift (expr ids) #:transparent)
-(define-struct local-lift-end (decl) #:transparent)
-(define-struct local-lift-require (req expr mexpr) #:transparent)
-(define-struct local-lift-provide (prov) #:transparent)
-(define-struct local-bind (names ?1 renames bindrhs) #:transparent)
-(define-struct local-value (name ?1 resolves bound? binding) #:transparent)
-  ;; binding is saved (identifier-binding name) at time of lookup, since it may change
-  ;; if name is rebound in definition context
-(define-struct track-origin (before after) #:transparent)
-(define-struct local-remark (contents) #:transparent)
-  ;; contents : (listof (U string syntax))
-(define-struct local-mess (events) #:transparent)
+;; - (local-exn Exn)
+;; - (local-expansion Stx Stx Bool Stx Deriv ??? Stx Opaque/#f)
+;; - (local-lift Stxs Ids)
+;; - (local-lift-end Stx)
+;; - (local-lift-require Stx Stx Stx)
+;; - (local-lift-provide Stx)
+;; - (local-bind Ids ?Exn Stx Stx) -- FIXME: renames structure?
+;; - (local-value Id ?Exn Ids Bool IdentifierBinding)
+;; - (track-origin Stx Stx)
+;; - (local-remark (Listof (U String Syntax)))
+;; - (local-mess (Listof Event))
+(struct local-exn (exn) #:transparent)
+(struct local-expansion node (for-stx? me1 inner lifted me2 opaque) #:transparent)
+(struct local-lift (expr ids) #:transparent)
+(struct local-lift-end (decl) #:transparent)
+(struct local-lift-require (req expr mexpr) #:transparent)
+(struct local-lift-provide (prov) #:transparent)
+(struct local-bind (names ?1 renames bindrhs) #:transparent)
+(struct local-value (name ?1 resolves bound? binding) #:transparent)
+;;   binding is saved (identifier-binding name) at time of lookup, since it may change
+;;   if name is rebound in definition context
+(struct track-origin (before after) #:transparent)
+(struct local-remark (contents) #:transparent)
+(struct local-mess (events) #:transparent)
 
-;; A PrimDeriv is one of
-(define-struct (prule base) () #:transparent)
-(define-struct (p:variable prule) () #:transparent)
 
-;;   (make-p:module <Base> PrepareEnv stxes ?Deriv ?exn ?stx ?Deriv ?exn Deriv ?stx)
-;;   (make-p:#%module-begin <Base> Stx ModuleBegin/Phase ?exn)
-(define-struct (p:module prule) (prep rename check1 ?2 tag2 check2 ?3 body shift)
-  #:transparent)
-(define-struct (p:#%module-begin prule) (me pass12 ?2 pass3 ?3 pass4) #:transparent)
+;; ============================================================
+;; Primitives
 
-;;   (make-p:define-syntaxes <Base> (listof LocalAction) DerivLL (listof LocalAction))
-;;   (make-p:define-values <Base> Deriv)
-(define-struct (p:define-syntaxes prule) (prep rhs locals) #:transparent)
-(define-struct (p:define-values prule) (rhs) #:transparent)
+;; A PrimDeriv <: Base is one of the following:
+(struct prule base () #:transparent)
 
-;;   (make-p:#%expression <Base> Deriv ?Stx)
-;;   (make-p:if <Base> Boolean Deriv Deriv Deriv)
-;;   (make-p:wcm <Base> Deriv Deriv Deriv)
-;;   (make-p:set! <Base> Rs ?Exn Deriv)
-;;   (make-p:set!-macro <Base> Rs Deriv)
-(define-struct (p:#%expression prule) (inner untag) #:transparent)
-(define-struct (p:if prule) (test then else) #:transparent)
-(define-struct (p:wcm prule) (key mark body) #:transparent)
-(define-struct (p:set! prule) (id-resolves ?2 rhs) #:transparent)
-(define-struct (p:set!-macro prule) (deriv) #:transparent)
+;; - p:variable is (p:variable <Base>)
+(struct p:variable prule () #:transparent)
 
-;;   (make-p:#%app <Base> Stx Derivs)
-;;   (make-p:begin <Base> Derivs)
-;;   (make-p:begin0 <Base> Derivs)
-(define-struct (p:#%app prule) (derivs) #:transparent)
-(define-struct (p:begin prule) (derivs) #:transparent)
-(define-struct (p:begin0 prule) (derivs) #:transparent)
+;; - (p:module <Base> PrepareEnv Stx Deriv ?Exn Stx Deriv ?Exn Deriv Stx)
+(struct p:module prule (prep rename check1 ?2 tag2 check2 ?3 body shift) #:transparent)
 
-;;   (make-p:lambda <Base> LambdaRenames BDeriv)
-;;   (make-p:case-lambda <Base> (list-of CaseLambdaClause))
-;;   (make-p:let-values <Base> LetRenames (list-of Deriv) BDeriv)
-;;   (make-p:letrec-values <Base> LetRenames (list-of Deriv) BDeriv)
-;;   (make-p:letrec-syntaxes+values <Base> LSVRenames PrepareExpEnv
-;;      (list-of BindSyntaxes) (list-of Deriv) BDeriv)
-(define-struct (p:lambda prule) (renames body) #:transparent)
-(define-struct (p:case-lambda prule) (renames+bodies) #:transparent)
-(define-struct (p:let-values prule) (renames rhss body) #:transparent)
-(define-struct (p:letrec-values prule) (renames rhss body) #:transparent)
-(define-struct (p:letrec-syntaxes+values prule)
-  (srenames prep sbindrhss vrhss body)
-  #:transparent)
+;; - (p:#%module-begin <Base> Stx ModPass1/2 ?Exn ModPass3 ?Exn ModPass4)
+(struct p:#%module-begin prule (me pass12 ?2 pass3 ?3 pass4) #:transparent)
 
-;;   (make-p:provide <Base> (listof Deriv) ?exn)
-(define-struct (p:provide prule) (inners ?2) #:transparent)
+;; - (p:define-syntaxes <Base> LocalActions DerivLL LocalActions)
+(struct p:define-syntaxes prule (prep rhs locals) #:transparent)
 
-;;   (make-p:require <Base> (listof LocalAction))
-(define-struct (p:require prule) (locals) #:transparent)
+;; - (p:define-values <Base> Deriv)
+(struct p:define-values prule (rhs) #:transparent)
 
-(define-struct (p:submodule prule) (exp locals) #:transparent)
-(define-struct (p:submodule* prule) (exp locals) #:transparent)
+;; - (p:#%expression <Base> Deriv Stx/#f)
+(struct p:#%expression prule (inner untag) #:transparent)
 
-;;   (make-p:#%stratified-body <Base> BDeriv)
-(define-struct (p:#%stratified-body prule) (bderiv) #:transparent)
+;; - (p:if <Base> Deriv Deriv Deriv)
+(struct p:if prule (test then else) #:transparent)
+;; - (p:wcm <Base> Deriv Deriv Deriv)
+(struct p:wcm prule (key mark body) #:transparent)
+;; - (p:set! <Base> Resolves ?Exn Deriv)
+(struct p:set! prule (id-rs ?2 rhs) #:transparent)
+;; - (p:set!-macro <Base> Deriv)
+(struct p:set!-macro prule (deriv) #:transparent)
 
-;;   (make-p:begin-for-syntax <base> (listof LocalAction) BFSBody)
-;;     where BFSBody is one of
-;;       - ModuleBegin/Phase
-;;       - (list BeginForSyntaxLifts ... LDeriv))
-(define-struct (p:begin-for-syntax prule) (prep body locals) #:transparent)
+;; - (p:#%app <Base> Derivs)
+(struct p:#%app prule (derivs) #:transparent)
+;; - (p:begin <Base> Derivs)
+(struct p:begin prule (derivs) #:transparent)
+;; - (p:begin0 <Base> Derivs)
+(struct p:begin0 prule (derivs) #:transparent)
 
-;;   (make-p:stop <Base>)
-;;   (make-p:unknown <Base>)
-;;   (make-p:#%top <Base> Stx)
-;;   (make-p:#%datum <Base> Stx)
-;;   (make-p:quote <Base>)
-;;   (make-p:quote-syntax <Base>)
-;;   (make-p:#%variable-reference <Base>)
-(define-struct (p::STOP prule) () #:transparent)
-(define-struct (p:stop p::STOP) () #:transparent)
-(define-struct (p:unknown p::STOP) () #:transparent)
-(define-struct (p:#%top p::STOP) () #:transparent)
-(define-struct (p:#%datum p::STOP) () #:transparent)
-(define-struct (p:quote p::STOP) () #:transparent)
-(define-struct (p:quote-syntax p::STOP) () #:transparent)
-(define-struct (p:#%variable-reference p::STOP) () #:transparent)
+;; - (p:lambda <Base> LambdaRenames BDeriv)
+(struct p:lambda prule (renames body) #:transparent)
+;; - (p:case-lambda <Base> CaseLambdaClauses)
+;;   where a CaseLambdaClause is (clc ?Exn CaseLambdaRename BDeriv)
+(struct p:case-lambda prule (renames+bodies) #:transparent)
+(struct clc (?1 renames body) #:transparent)
 
-;;   (p:declare <Base>)
+;; LambdaRenames = (list* Stx Stxs)    -- renamed (formals . body)
+;; LetRenames = (list* Stxs Stxs)      -- renamed (((vars rhs) ...) . body)
+;; LSVRenames = (list* Stxs Stxs Stxs) -- renamed (((svars srhs) ...) ((vvars vrhs) ...) . body)
+
+;; - (p:let-values <Base> LetRenames (list-of Deriv) BDeriv)
+(struct p:let-values prule (renames rhss body) #:transparent)
+;; - (p:letrec-values <Base> LetRenames (list-of Deriv) BDeriv)
+(struct p:letrec-values prule (renames rhss body) #:transparent)
+;; - (p:letrec-syntaxes+values <Base> LSVRenames PrepareExpEnv BindSyntaxes-s Derivs BDeriv)
+(struct p:letrec-syntaxes+values prule (srenames prep sbindrhss vrhss body) #:transparent)
+
+;; - (p:provide <Base> Derivs ?Exn)
+(struct p:provide prule (inners ?2) #:transparent)
+
+;; - (p:require <Base> LocalActions)
+(struct p:require prule (locals) #:transparent)
+
+;; - (p:submodule <Base> p:module LocalActions)
+(struct p:submodule prule (exp locals) #:transparent)
+;; - (p:submodule* <Base> p:module LocalActions)
+(struct p:submodule* prule (exp locals) #:transparent)
+
+;; - (p:#%stratified-body <Base> BDeriv)
+(struct p:#%stratified-body prule (bderiv) #:transparent)
+
+;; - (p:begin-for-syntax <Base> LocalActions BFSBody)
+;;   where BFSBody = ModuleBegin/Phase | (list BeginForSyntaxLifts ... LDeriv)
+(struct p:begin-for-syntax prule (prep body locals) #:transparent)
+
+;; - (p:stop <Base>)
+;; - (p:unknown <Base>)
+;; - (p:#%top <Base>)
+;; - (p:#%datum <Base>)
+;; - (p:quote <Base>)
+;; - (p:quote-syntax <Base>)
+;; - (p:#%variable-reference <Base>)
+(struct p::STOP prule () #:transparent)
+(struct p:stop p::STOP () #:transparent)
+(struct p:unknown p::STOP () #:transparent)
+(struct p:#%top p::STOP () #:transparent)
+(struct p:#%datum p::STOP () #:transparent)
+(struct p:quote p::STOP () #:transparent)
+(struct p:quote-syntax p::STOP () #:transparent)
+(struct p:#%variable-reference p::STOP () #:transparent)
+
+;; - (p:declare <Base>)
 (struct p:declare prule () #:transparent)
 
-;; A LDeriv is
-;;   (make-lderiv <Node(Stxs)> ?exn (list-of Deriv))
-(define-struct (lderiv node) (?1 derivs) #:transparent)
 
-;; A BDeriv is
-;;   (make-bderiv <Node(Stxs)> BlockRenames (list-of BRule) (U LDeriv BlockLetrec))
-(define-struct (bderiv node) (renames pass1 pass2) #:transparent)
+;; ============================================================
+;; Blocks
 
-;; A BlockLetrec is
-;;   (block:letrec Syntax (Listof Deriv) LDeriv)
-(define-struct block:letrec (stx rhss lderiv) #:transparent)
+;; A LDeriv is (lderiv Stxs Stxs ?Exn Derivs)
+(struct lderiv node (?1 derivs) #:transparent)
+
+;; A BDeriv is (bderiv Stxs Stxs BlockRenames BRules (U LDeriv BlockLetrec))
+;; where BlockRenames = (list* Stxs Stxs) -- (list* init-stxs renamed-stxs)
+(struct bderiv node (renames pass1 pass2) #:transparent)
+
+;; A BlockLetrec is (block:letrec Stx Derivs LDeriv)
+(struct block:letrec (stx rhss lderiv) #:transparent)
 
 ;; A BRule is one of
-;;   (make-b:error exn)
-;;   (make-b:expr Deriv)
-;;   (make-b:splice Deriv ?exn Stxs ?exn)
-;;   (make-b:defvals Deriv ?exn Stx ?exn)
-;;   (make-b:defstx Deriv ?exn Stx ?exn PrepareExpEnv BindSyntaxes)
-(define-struct brule () #:transparent)
-(define-struct (b:error brule) (?1) #:transparent)
-(define-struct (b:expr brule) (head) #:transparent)
-(define-struct (b:splice brule) (head ?1 tail ?2) #:transparent)
-(define-struct (b:defvals brule) (head ?1 rename ?2) #:transparent)
-(define-struct (b:defstx brule) (head ?1 rename ?2 prep bindrhs) #:transparent)
+;; - (b:error exn)
+;; - (b:expr Deriv)
+;; - (b:splice Deriv ?Exn Stxs ?Exn)
+;; - (b:defvals Deriv ?Exn Stx ?Exn)
+;; - (b:defstx Deriv ?Exn Stx ?Exn PrepareExpEnv BindSyntaxes)
+(struct brule () #:transparent)
+(struct b:error brule (?1) #:transparent)
+(struct b:expr brule (head) #:transparent)
+(struct b:splice brule (head ?1 tail ?2) #:transparent)
+(struct b:defvals brule (head ?1 rename ?2) #:transparent)
+(struct b:defstx brule (head ?1 rename ?2 prep bindrhs) #:transparent)
 
-;; A BindSyntaxes is
-;;   (make-bind-syntaxes DerivLL (listof LocalAction))
-(define-struct bind-syntaxes (rhs locals) #:transparent)
 
-;; A CaseLambdaClause is
-;;   (make-clc ?exn CaseLambdaRename BDeriv)
-(define-struct clc (?1 renames body) #:transparent)
-
-;; A BlockRename is (cons Stx Stx)
-
-;; A BeginForSyntaxLifts is
-;;   (make-bfs:lift LDeriv (listof stx))
-(define-struct bfs:lift (lderiv lifts) #:transparent)
+;; ============================================================
+;; Modules
 
 ;; A ModuleBegin/Phase is (module-begin/phase ModulePass1 ModulePass2 ModulePass3)
-(define-struct module-begin/phase (pass1 pass2 pass3) #:transparent)
+(struct module-begin/phase (pass1 pass2 pass3) #:transparent)
 
 ;; A ModPass1 is (list-of ModRule1)
 ;; A ModPass2 is (list-of ModRule2)
@@ -220,7 +238,17 @@
 ;; - p:submodule*
 
 
-;; ECTE represents expand/compile-time-evals
-;; (make-ecte stx ?stx (listof LocalAction) Deriv Deriv (listof LocalAction))
+;; ============================================================
+;; Misc
 
-(define-struct (ecte deriv) (locals first second locals2) #:transparent)
+;; type PrepareExpEnv = LocalActions
+
+;; A BindSyntaxes is (bind-syntaxes DerivLL LocalActions)
+(struct bind-syntaxes (rhs locals) #:transparent)
+
+;; A BeginForSyntaxLifts is (make-bfs:lift LDeriv Stxs)
+(struct bfs:lift (lderiv lifts) #:transparent)
+
+;; An ECTE is (ecte Stx Stx LocalActions Deriv Deriv LocalActions)
+;; represents expand/compile-time-evals
+(struct ecte deriv (locals first second locals2) #:transparent)
