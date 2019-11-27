5807303568b2aa30aa60bc7eb4263f334f269385
Ryan Culpepper <ryanc@racket-lang.org>
2019-11-27 20:29:00 +0100

    ... WIP model/stx-util.rkt, disarm and resyntax helpers

--- macro-debugger-text-lib/macro-debugger/model/stx-util.rkt
+++ macro-debugger-text-lib/macro-debugger/model/stx-util.rkt
@@ -1,6 +1,9 @@
 #lang racket/base
 (provide stx->datum
-         syntaxish?)
+         syntaxish?
+         stx-disarm
+         resyntax
+         restx)
 
 (define (stx->datum x)
   (syntax->datum (datum->syntax #f x)))
@@ -11,3 +14,19 @@
       (and (pair? x)
            (syntaxish? (car x))
            (syntaxish? (cdr x)))))
+
+(define insp
+  (variable-reference->module-declaration-inspector
+   (#%variable-reference)))
+
+(define (stx-disarm stx)
+  (if (syntax? stx) (syntax-disarm stx insp) stx))
+
+(define (resyntax v stx [dstx (stx-disarm stx)] #:rearm? [rearm? #t])
+  (unless (and (syntax? stx) (syntax? dstx))
+    (error 'resyntax "not syntax: ~e, ~e" stx dstx))
+  (define stx* (datum->syntax dstx v stx stx))
+  (if rearm? (syntax-rearm stx* stx) stx*))
+
+(define (restx v stx [dstx (stx-disarm stx)] #:rearm? [rearm? #t])
+  (if (syntax? stx) (resyntax v stx dstx #:rearm? rearm?) v))
