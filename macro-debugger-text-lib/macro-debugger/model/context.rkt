f1178343fb8b982318b5e8b7c6a2943f701f1714
Ryan Culpepper <ryanc@racket-lang.org>
2019-11-27 18:39:55 +0100

    ... WIP model/context.rkt

--- macro-debugger-text-lib/macro-debugger/model/context.rkt
+++ macro-debugger-text-lib/macro-debugger/model/context.rkt
@@ -1,12 +1,19 @@
 #lang racket/base
-(require syntax/stx)
+(require racket/match
+         syntax/stx)
 (provide path-add-ref
          path-add-tail
+         path-add-car
+         path-add-cdr
          path-get
-         path-replace)
+         path-replace
+         path-replacer)
 
 ;; A Path is a (list-of PathSeg), where the PathSegs are listed outermost to innermost.
 ;; For example: (path-get #'((a b) (c d)) (list (ref 0) (ref 1))) = #'b, not #'c
+
+;; Paths are reversible (they can be built in accumulators), but as a consequence they
+;; have non-canonical forms: for example, (list (ref 2)) = (list (tail 1) (ref 0)).
 
 ;; A PathSeg is one of
 ;; - Nat -- represents nth car
@@ -18,7 +25,12 @@
 (define (tail? x) (and (exact-integer? x) (negative? x)))
 (define (tail-n x) (sub1 (- x)))
 (define (path-add-ref n p) (cons (ref n) p))
-(define (path-add-tail n p) (cons (tail n) p))
+(define (path-add-tail n p)
+  (match p
+    [(cons (? tail t) p*) (cons (tail (+ n (tail-n t))) p*)]
+    [_ (cons (tail n) p)]))
+(define (path-add-car p) (path-add-ref 0 p))
+(define (path-add-cdr p) (path-add-tail 0 p))
 
 ;; path-get : syntax Path -> syntax
 (define (path-get stx path0)
@@ -42,8 +54,8 @@
                  [(zero? n) (stx-cdr stx)]
                  [else (loop (sub1 n) (stx-cdr stx))]))]))
 
-;; path-replace : syntax Path syntax -> syntax
-(define (path-replace stx path x)
+;; path-replace : Syntax Path Syntax [Boolean] -> syntax
+(define (path-replace stx path x #:resyntax? [resyntax? #t])
   (cond [(null? path) x]
         [(pair? path)
          (let ([pathseg0 (car path)])
@@ -51,38 +63,45 @@
                             pathseg0
                             (path-replace (pathseg-get stx pathseg0)
                                           (cdr path)
-                                          x)))]
+                                          x
+                                          resyntax?)
+                            resyntax?))]
         [else (error 'path-replace "bad path: ~s" path)]))
 
-;; pathseg-replace : syntax PathSeg syntax -> syntax
-(define (pathseg-replace stx0 pathseg x)
+;; pathseg-replace : Syntax PathSeg Syntax Boolean -> syntax
+(define (pathseg-replace stx0 pathseg x resyntax?)
   (cond [(ref? pathseg)
          (let loop ([n (ref-n pathseg)] [stx stx0])
            (cond [(not (stx-pair? stx))
                   (error 'pathseg-replace "path out of bounds: ~s, ~s" pathseg stx0)]
-                 [(zero? n) (stx-replcar stx x)]
-                 [else (stx-replcdr stx (loop (sub1 n) (stx-cdr stx)))]))]
+                 [(zero? n) (stx-replcar stx x resyntax?)]
+                 [else (stx-replcdr stx (loop (sub1 n) (stx-cdr stx)) resyntax?)]))]
         [(tail? pathseg)
          (let loop ([n (tail-n pathseg)] [stx stx0])
            (cond [(not (stx-pair? stx))
                   (error 'pathseg-replace "path out of bounds: ~s, ~s" pathseg stx0)]
-                 [(zero? n) (stx-replcdr stx x)]
-                 [else (stx-replcdr stx (loop (sub1 n) (stx-cdr stx)))]))]))
+                 [(zero? n) (stx-replcdr stx x resyntax?)]
+                 [else (stx-replcdr stx (loop (sub1 n) (stx-cdr stx)) resyntax?)]))]))
 
 ;; stx-replcar : syntax syntax -> syntax
-(define (stx-replcar stx x)
+(define (stx-replcar stx x resyntax?)
   (cond [(pair? stx)
          (cons x (cdr stx))]
         [(and (syntax? stx) (pair? (syntax-e stx)))
          (let ([dstx (syntax-disarm stx #f)])
-           (syntax-rearm (datum->syntax dstx (cons x (cdr (syntax-e dstx))) stx stx) stx))]
+           (let ([result (cons x (cdr (syntax-e dstx)))])
+             (if resyntax? (syntax-rearm (datum->syntax dstx result stx stx) stx) result)))]
         [else (raise-type-error 'stx-replcar "stx-pair" stx)]))
 
 ;; stx-replcdr : syntax syntax -> syntax
-(define (stx-replcdr stx x)
+(define (stx-replcdr stx x resyntax?)
   (cond [(pair? stx)
          (cons (car stx) x)]
         [(and (syntax? stx) (pair? (syntax-e stx)))
          (let ([dstx (syntax-disarm stx #f)])
-           (syntax-rearm (datum->syntax dstx (cons (car (syntax-e dstx)) x) stx stx) stx))]
+           (let ([result (cons (car (syntax-e dstx)) x)])
+             (if resyntax? (syntax-rearm (datum->syntax dstx result stx stx) stx) result)))]
         [else (raise-type-error 'stx-replcdr "stx-pair" stx)]))
+
+(define ((path-replacer stx path) s #:resyntax? [resyntax? #t])
+  (path-replace stx path s #:resyntax? resyntax?))
