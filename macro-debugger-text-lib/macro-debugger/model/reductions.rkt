420c5012b55db1f24bee316bd7c3ff1911c2b8de
Ryan Culpepper <ryanc@racket-lang.org>
2019-12-03 02:02:05 +0100

    ... wrap strict checks

--- macro-debugger-text-lib/macro-debugger/model/reductions.rkt
+++ macro-debugger-text-lib/macro-debugger/model/reductions.rkt
@@ -64,10 +64,11 @@
     [(deriv e1 e2)
      (R [#:pattern ?form]
         [#:do (DEBUG (eprintf "\n>> ~.s\n" (stx->datum e1)))]
-        [#:do (unless (eq? (% ?form) e1)
-                (eprintf "MISMATCH: not eq\n  actual = ~.s\n  expect = ~.s\n"
-                         (stx->datum (% ?form)) (stx->datum e1))
-                (eprintf "  deriv = ~e\n" d))]
+        [#:do (STRICT-CHECKS
+               (unless (eq? (% ?form) e1)
+                 (eprintf "MISMATCH: not eq\n  actual = ~.s\n  expect = ~.s\n"
+                          (stx->datum (% ?form)) (stx->datum e1))
+                 (eprintf "  deriv = ~e\n" d)))]
         [#:when (not (eq? (% ?form) e1))
          [#:rename ?form e1 'sync]] ;; FIXME, neither sync nor #:set-syntax
         [#:parameterize ((the-context (add-rearm-frame e1 (the-context))))
